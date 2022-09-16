000dedd10a55b122fde4514289c42f3d24f31252
HolyCow <83113185+h0lycow@users.noreply.github.com>
2022-09-16 18:18:28 +0530

    DA

---
+++ Sem5/D2 Java/Thoery/DA1/Q1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Q1 {
+
+    public static int[] Ans(int arr[], int n) {
+        int max = -999, pos = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                pos = i;
+            }
+        }
+        int ans[] = { max, pos };
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Goodbye world");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        int Solution[] = Ans(arr, n);
+        System.out.println("Maximum element: " + Solution[0] + " at index: " + Solution[1]);
+        sc.close();
+    }
+}
