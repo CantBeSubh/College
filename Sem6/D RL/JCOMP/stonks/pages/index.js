4ccec23db30ba8252f8743405752da897d6cd9fb
Subhranshu Pati <subhranshupati0412@gmail.com>
2023-04-09 14:40:31 +0530

    GETTING JSON

--- Sem6/D RL/JCOMP/stonks/pages/index.js
+++ Sem6/D RL/JCOMP/stonks/pages/index.js
@@ -2,20 +2,25 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import cx from 'classnames';
 import { useEffect, useState } from 'react';
+import { saveAs } from 'file-saver';
+
 
 export default function Home() {
-  const [stockdata, setStockData] = useState(null);
-  const [company, setCompany] = useState([]);
-  const getData = async () => {
-    const res = await fetch('/api/stockdata', { method: 'GET' });
+  const [data, setData] = useState(null);
+
+  const getSymbol = async (search) => {
+    const res = await fetch(`/api/searchStockSymbol?search=${search}`);
     const data = await res.json();
-    setStockData(data);
+    return data;
   }
 
-  useEffect(() => {
-    getData();
-  }, []);
-
+  const storeData = async (symbol) => {
+    const res = await fetch(`/api/getStockData?symbol=${symbol}`);
+    const data = await res.json();
+    setData(data);
+    const blob = new Blob([JSON.stringify(data)], { type: 'application/json' });
+    saveAs(blob, `${symbol}.json`);
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -26,22 +31,11 @@ export default function Home() {
       <div className={cx(styles["form-signin"], "text-center", "mt-5")}>
 
         <form
-          onSubmit={(e) => {
+          onSubmit={async (e) => {
             e.preventDefault();
-            if (!stockdata) {
-              alert("Please wait for the <stockdata> to load")
-              return
-            }
-            const search = e.target[0].value;
-            if (!search) {
-              alert("Please enter a company's name");
-              return
-            }
-            for (let i of stockdata.data) {
-              if (i.name.toLowerCase().includes(search.toLowerCase())) {
-                setCompany([i, ...company]);
-              }
-            }
+            const symbol = await getSymbol(e.target[0].value);
+            await storeData(symbol);
+            console.log(symbol);
             e.target.reset();
           }}
         >
@@ -53,23 +47,6 @@ export default function Home() {
           </div>
           <button className={cx(styles["button"], "w-50", "btn", "btn-lg", "btn-outline-light")} type="submit">Submit</button>
           <div className='mt-3'>
-            {/* {company.length > 0 && <h3 className="text-light">Your Stocks</h3>} */}
-
-            {company &&
-              company.map((i) => {
-                return (
-                  <div className="card text-white ml-3 mb-3 mt-10" key={i.symbol}>
-                    <h4 className="card-header">{i.name}</h4>
-                    <div className="card-body">
-                      <h5 className="card-title">Stock Price: $ {Math.round(Math.random() * 100)}</h5>
-                      <p className="card-text">Stock Symbol: {i.symbol}</p>
-                      <p className="card-text">Stock Exchange: {i.exchange}</p>
-                    </div>
-                  </div>
-                )
-              }
-              )
-            }
 
           </div>
 
@@ -78,3 +55,21 @@ export default function Home() {
     </div>
   )
 }
+
+
+
+// {company &&
+//   company.map((i) => {
+//     return (
+//       <div className="card text-white ml-3 mb-3 mt-10" key={i.symbol}>
+//         <h4 className="card-header">{i.name}</h4>
+//         <div className="card-body">
+//           <h5 className="card-title">Stock Price: $ {Math.round(Math.random() * 100)}</h5>
+//           <p className="card-text">Stock Symbol: {i.symbol}</p>
+//           <p className="card-text">Stock Exchange: {i.exchange}</p>
+//         </div>
+//       </div>
+//     )
+//   }
+//   )
+// }
